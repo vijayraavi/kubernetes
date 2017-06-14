5a64b859f761b7f5f1fc6b8213c5ac43e726be67
Madhan Raj Mookkandy <madhanm@microsoft.com>
2017-06-14 16:27:39 -0700

    Following are part of this commit +++++++++++++++++++++++++++++++++ * Windows CNI Support 	(1) Support to use --network-plugin=cni 	(2) Handled platform requirement of calling CNI ADD for all the containers. 	(2.1) For POD Infra container, netNs has to be empty 	(2.2) For all other containers, sharing the network namespace of POD container, 	      should pass netNS name as "container:<Pod Infra Container Id>", same as the               NetworkMode of the current container 	(2.3) The Windows CNI plugin has to handle this to call into Platform.               Sample Windows CNI Plugin code to be shared soon. * Sandbox support for Windows 	(1) Sandbox support for Windows. Works only with Docker runtime. 	(2) Retained CONTAINER_NETWORK as a backward compatibilty flag, 	    to not break existing deployments using it. 	(3) Works only with CNI plugin enabled.
    (*) Sample Script to start Kubelet and KubeProxy
    (*) Changes to reinvoke CNI ADD for every new container created. This is hooked up with PodStatus,
        but would be ideal to move it outside of this, once we have CNI GET support
    
    From @madhanrm, kubernetes/kubernetes PR #51063

---
+++ cluster/windows/cni/kubeproxystart.ps1
@@ -0,0 +1,21 @@
+# Copyright 2016 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+#$env:INTERFACE_TO_ADD_SERVICE_IP="vEthernet (forwarder)"
+#c:\k\kube-proxy.exe --v=4 --proxy-mode=usermode --hostname-override=15992acs9000 --kubeconfig=c:\k\config
+
+$env:KUBE_NETWORK="l2bridge"
+c:\k\kube-proxy.exe --v=4 --proxy-mode=kernelspace --hostname-override=$(hostname) --kubeconfig=c:\k\config
--- pkg/kubelet/dockershim/helpers_unsupported.go
+++ pkg/kubelet/dockershim/helpers_unsupported.go
@@ -52,3 +52,11 @@ func (ds *dockerService) determinePodIPBySandboxID(uid string) string {
 func (ds *dockerService) configureInfraContainerNetworkConfig(containerID string) {
 	// Do nothing
 }
+
+func getNetworkNamespace(c *dockertypes.ContainerJSON) (string, error) {
+	if c.State.Pid == 0 {
+		// Docker reports pid 0 for an exited container.
+		return "", fmt.Errorf("Cannot find network namespace for the terminated container %q", c.ID)
+	}
+	return fmt.Sprintf(dockerNetNSFmt, c.State.Pid), nil
+}
