b4e08fbbf4d7971bd42c2da1b918a6c814def857
Jiangtian Li <jiangtianli@hotmail.com>
2017-07-26 15:31:20 -0700

    Fix network config due to the split of start POD sandbox and start container from 1.7.0

--- pkg/kubelet/dockershim/docker_sandbox.go
+++ pkg/kubelet/dockershim/docker_sandbox.go
@@ -119,8 +119,6 @@ func (ds *dockerService) RunPodSandbox(config *runtimeapi.PodSandboxConfig) (id 
 	}
 
 	// Windows specific workaround to configure networking post container creation.
-	// Uncomment below once we have network namespace sharing is available
-	//if container.Name == PodInfraContainerName {
 	ds.configureInfraContainerNetworkConfig(createResp.ID)
 
 	// Step 4: Start the sandbox container.
@@ -169,17 +167,6 @@ func (ds *dockerService) RunPodSandbox(config *runtimeapi.PodSandboxConfig) (id 
 		}
 	}
 
-	// Windows specific workaround to configure networking post container creation.
-	dns := ""
-	if dnsConfig := config.GetDnsConfig(); dnsConfig != nil {
-		if len(dnsConfig.Servers) > 0 {
-			dns = dnsConfig.Servers[0]
-		}
-	}
-
-	// Uncomment below once we have network namespace sharing is available
-	//if container.Name == PodInfraContainerName {
-	ds.FinalizeInfraContainerNetwork(createResp.ID, dns)
 	return createResp.ID, err
 }
 
