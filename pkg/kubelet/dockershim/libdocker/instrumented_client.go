f87965c3895a68a7321d96431ea3a24646dbdd21
George Kudrayvtsev <gekudray@microsoft.com>
2017-09-15 18:20:24 -0700

    Because HNS properly configures network policy as of this patch, the following commits are no longer needed and are reverted in this commit (ordered by newest -> oldest):
      - "Use dns policy to determine setting DNS servers on the correct NIC in Windows container"
        `76b94898ec0063f597961c9957e9e6391140fb59`
    
      - "Fix network config due to the split of start POD sandbox and start container from 1.7.0"
        `74a2f37447ac93ba005a01f009ce0f8db1ef69ef`
    
      - "Workaround for Outbound Internet traffic in Azure Kubernetes"
        `5fc0a5e4a2dac8b03dd42134c13a4b3dc0dd6d0c`

--- pkg/kubelet/dockershim/libdocker/instrumented_client.go
+++ pkg/kubelet/dockershim/libdocker/instrumented_client.go
@@ -20,7 +20,6 @@ import (
 	"time"
 
 	dockertypes "github.com/docker/engine-api/types"
-        dockernetworktypes "github.com/docker/engine-api/types/network"
 	"k8s.io/kubernetes/pkg/kubelet/metrics"
 )
 
@@ -104,15 +103,6 @@ func (in instrumentedInterface) RemoveContainer(id string, opts dockertypes.Cont
 	defer recordOperation(operation, time.Now())
 
 	err := in.client.RemoveContainer(id, opts)
-	recordError(operation, err)
-	return err
-}
-
-func (in instrumentedInterface) ConnectNetwork(id string, containerID string, config *dockernetworktypes.EndpointSettings) error {
-	const operation = "connect_network"
-	defer recordOperation(operation, time.Now())
-
-	err := in.client.ConnectNetwork(id, containerID, config)
 	recordError(operation, err)
 	return err
 }
