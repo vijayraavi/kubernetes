1f0ccc17a9538a2592ef534c8dd5866753e4c6eb
ACS CI <containers@microsoft.com>
2018-04-05 23:46:08 +0000

    Revert "Lock subPath volumes"
    This reverts commit 63b4f60e43afb8e7ddf6497cf7f77855206750e3.

--- pkg/volume/util/operationexecutor/operation_generator.go
+++ pkg/volume/util/operationexecutor/operation_generator.go
@@ -18,7 +18,6 @@ package operationexecutor
 
 import (
 	"fmt"
-	"path"
 	"time"
 
 	"github.com/golang/glog"
@@ -78,7 +77,7 @@ type OperationGenerator interface {
 	GenerateMountVolumeFunc(waitForAttachTimeout time.Duration, volumeToMount VolumeToMount, actualStateOfWorldMounterUpdater ActualStateOfWorldMounterUpdater, isRemount bool) (func() error, string, error)
 
 	// Generates the UnmountVolume function needed to perform the unmount of a volume plugin
-	GenerateUnmountVolumeFunc(volumeToUnmount MountedVolume, actualStateOfWorld ActualStateOfWorldMounterUpdater, podsDir string) (func() error, string, error)
+	GenerateUnmountVolumeFunc(volumeToUnmount MountedVolume, actualStateOfWorld ActualStateOfWorldMounterUpdater) (func() error, string, error)
 
 	// Generates the AttachVolume function needed to perform attach of a volume plugin
 	GenerateAttachVolumeFunc(volumeToAttach VolumeToAttach, actualStateOfWorld ActualStateOfWorldAttacherUpdater) (func() error, string, error)
@@ -503,8 +502,7 @@ func (og *operationGenerator) GenerateMountVolumeFunc(
 
 func (og *operationGenerator) GenerateUnmountVolumeFunc(
 	volumeToUnmount MountedVolume,
-	actualStateOfWorld ActualStateOfWorldMounterUpdater,
-	podsDir string) (func() error, string, error) {
+	actualStateOfWorld ActualStateOfWorldMounterUpdater) (func() error, string, error) {
 	// Get mountable plugin
 	volumePlugin, err :=
 		og.volumePluginMgr.FindPluginByName(volumeToUnmount.PluginName)
@@ -519,14 +517,6 @@ func (og *operationGenerator) GenerateUnmountVolumeFunc(
 	}
 
 	return func() error {
-		mounter := og.volumePluginMgr.Host.GetMounter(volumeToUnmount.PluginName)
-
-		// Remove all bind-mounts for subPaths
-		podDir := path.Join(podsDir, string(volumeToUnmount.PodUID))
-		if err := mounter.CleanSubPaths(podDir, volumeToUnmount.OuterVolumeSpecName); err != nil {
-			return volumeToUnmount.GenerateErrorDetailed("error cleaning subPath mounts", err)
-		}
-
 		// Execute unmount
 		unmountErr := volumeUnmounter.TearDown()
 		if unmountErr != nil {
